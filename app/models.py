8a5df0e1afde9a1bc795ed4df57539f5fba1e3d0
Julie Qiu <julieyeqiu@gmail.com>
2015-08-05 18:24:52 -0400

    mouseover

--- app/models.py
+++ app/models.py
@@ -1,20 +1,36 @@
 from app import db
 
+tweets_locations = db.Table('tags',
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('tweet_id', db.Integer, db.ForeignKey('tweets.id')),
+    db.Column('location_id', db.Integer, db.ForeignKey('locations.id')),
+    db.UniqueConstraint('tweet_id', 'location_id', name='tags_tweet_id_location_id')
+)
+
 class Tweets(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    t_url = db.Column(db.String(120), index = True)
+    t_url = db.Column(db.String(120), index = True, unique=True)
     headline = db.Column(db.String(300), index=True)
-    t_location = db.Column(db.String(120), db.ForeignKey('locations.id'))
-
+                    
     def __repr__(self):
         return '<Tweets %r>' % (self.t_url)
+
+    def add_location(self, location):
+        self.place.append(location)
+        return self
 
 class Locations(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     location = db.Column(db.String(120), index=True, unique=True)
     latitude = db.Column(db.Integer, unique=False)
     longitude = db.Column(db.Integer, unique=False)
-    tweets = db.relationship('Tweets', backref='place', lazy='dynamic')
-
+    tags = db.relationship('Tweets', 
+                    secondary=tweets_locations, 
+                    backref=db.backref('place', 
+                            cascade="all", lazy='dynamic'))
+    
     def __repr__(self):
-        return '<User %r>' % (self.location)
+        return '<Locations %r>' % (self.location)
+
+    def get_tweets(self):
+        return self.tags
--- test.py
+++ test.py
@@ -1,2 +1,16 @@
 from app import app
 import app.news
+from app import models, db
+
+
+#tweets = models.Tweets.query.all()
+#locations = models.Locations.query.all()
+
+#for tweet in tweets:
+#    print tweet
+
+
+#app.news.remove_tweets_from_database()
+#app.news.add_cities_to_database()
+app.news.add_tweets_to_database()
+
